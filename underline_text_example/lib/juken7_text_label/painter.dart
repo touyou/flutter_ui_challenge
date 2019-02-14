ef12971bbe840683e57d6756df10e3f90a42f962
touyou <fujiyou1121@gmail.com>
2019-02-14 23:40:37 +0900

    fix painter code

--- underline_text_example/lib/juken7_text_label/painter.dart
+++ underline_text_example/lib/juken7_text_label/painter.dart
@@ -11,23 +11,23 @@ class UnderlineTextPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     var originList = _textPaint(canvas, size, this.texts, this.textStyle);
 
-    // labelPainter.paint(canvas, Offset(0, 0));
+    final Paint underlinePaint = new Paint()
+      ..style = PaintingStyle.fill
+      ..color = this.color;
+    var underlineHeight = originList[0].height / 2;
 
-    // var lineNumber = labelPainter.maxLines;
-
-    // final Paint underlinePaint = new Paint()
-    //   ..style = PaintingStyle.fill
-    //   ..color = this.color;
-    // canvas.drawRect(
-    //     Rect.fromLTRB(0, labelPainter.height - 5.0, labelPainter.width,
-    //         labelPainter.height),
-    //     underlinePaint);
+    for (final origin in originList) {
+      canvas.drawRect(
+          Rect.fromLTRB(0, origin.height - underlineHeight, origin.width,
+              underlineHeight),
+          underlinePaint);
+    }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
 
-  List<double> _textPaint(
+  List<TextSize> _textPaint(
       Canvas canvas, Size size, List<String> _texts, TextStyle _textStyle) {
     var lines = _texts.length;
     var _initialLinePainter = TextPainter();
@@ -39,10 +39,10 @@ class UnderlineTextPainter extends CustomPainter {
     _initialLinePainter.paint(canvas, Offset(0, 0));
 
     if (lines == 1) {
-      return [lineHeight];
+      return [TextSize(_initialLinePainter.width, lineHeight)];
     }
     _texts.removeAt(0);
-    var originList = [lineHeight];
+    var originList = [TextSize(_initialLinePainter.width, lineHeight)];
 
     for (final text in _texts) {
       var _labelPainter = TextPainter();
@@ -52,9 +52,16 @@ class UnderlineTextPainter extends CustomPainter {
 
       _labelPainter.paint(canvas, Offset(0, lineHeight + 5));
       lineHeight += 5 + _labelPainter.height;
-      originList.add(lineHeight);
+      originList.add(TextSize(_labelPainter.width, lineHeight));
     }
 
     return originList;
   }
+}
+
+class TextSize {
+  TextSize(this.width, this.height);
+
+  final double width;
+  final double height;
 }
