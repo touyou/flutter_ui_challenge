f14277ca8bb73018b3bab5d418a747e5d92538ef
touyou <fujiyou1121@gmail.com>
2019-02-14 23:22:49 +0900

    update painter

--- underline_text_example/lib/juken7_text_label/painter.dart
+++ underline_text_example/lib/juken7_text_label/painter.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class UnderlineTextPainter extends CustomPainter {
-  UnderlineTextPainter(this.texts, this.color) : super();
+  UnderlineTextPainter(this.texts, this.textStyle, this.color) : super();
 
   final List<String> texts;
+  final TextStyle textStyle;
   final Color color;
 
   @override
   void paint(Canvas canvas, Size size) {
-    var originList = _textPaint(canvas, size, this.texts);
+    var originList = _textPaint(canvas, size, this.texts, this.textStyle);
 
     // labelPainter.paint(canvas, Offset(0, 0));
 
@@ -26,9 +27,34 @@ class UnderlineTextPainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
 
-  List<int> _textPaint(Canvas canvas, Size size, List<String> _texts) {
+  List<double> _textPaint(
+      Canvas canvas, Size size, List<String> _texts, TextStyle _textStyle) {
     var lines = _texts.length;
+    var _initialLinePainter = TextPainter();
+    _initialLinePainter
+      ..text = TextSpan(style: _textStyle, text: _texts[0])
+      ..layout();
+    var lineHeight = _initialLinePainter.height;
 
-    return [];
+    _initialLinePainter.paint(canvas, Offset(0, 0));
+
+    if (lines == 1) {
+      return [lineHeight];
+    }
+    _texts.removeAt(0);
+    var originList = [lineHeight];
+
+    for (final text in _texts) {
+      var _labelPainter = TextPainter();
+      _labelPainter
+        ..text = TextSpan(style: _textStyle, text: text)
+        ..layout();
+
+      _labelPainter.paint(canvas, Offset(0, lineHeight + 5));
+      lineHeight += 5 + _labelPainter.height;
+      originList.add(lineHeight);
+    }
+
+    return originList;
   }
 }
